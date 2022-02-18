78e9cd46db0b540cf5b99b657ceb166877a0e063
Maksym.Mazurok <maksym.mazurok.ext@blablacar.com>
2022-02-18 14:40:39 +0200

    loggging

--- routing.py
+++ routing.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import time
+import logging
 
 import bigquery.utils as bu
 import gcs.utils as gu
@@ -24,18 +25,20 @@ def init_routing(api, app):
             try:
                 quotedata = bu.get_random_quote()
                 ieu.text_overlay(images[random.randint(0, len(images) - 1)], quotedata)
+                logging.info("Image generated")
             except:
-                print(sys.exc_info())
+                logging.error(sys.exc_info())
 
         insta_client = ic.Instagram()
 
         images_to_post = gu.list_bucket_objects("images_ready_to_post")
         for im in images_to_post:
             time.sleep(random.randint(60, 75))
-            print(im)
             media = insta_client.create_media(im)
-            print(media)
+
             insta_client.post_media(media)
+            logging.info("Image posted")
 
         gu.clean_bucket("images_ready_to_post")
+        logging.info("Bucket cleaned")
         return "published"
